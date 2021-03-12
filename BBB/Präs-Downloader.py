9ca8147b75b89526789d88fa9174d5fc6d08c70a
Malte Jürgens <maltejur@web.de>
2021-03-12 15:21:54 +0100

    Filepicker und besserer url input

--- BBB/Präs-Downloader.py
+++ BBB/Präs-Downloader.py
@@ -2,18 +2,29 @@ import cairosvg
 import io
 from PyPDF2 import PdfFileMerger
 import urllib
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
 i = 1
 merger = PdfFileMerger()
-url = input("URL with the ending '/svg/{}': ")
-name = input("Name of the file: ")
+split = input("URL: ").split("/")
+split.pop()
+url = "/".join(split) + "/{}"
+name = filedialog.asksaveasfile(
+    filetypes=[("PDF", "*.pdf")], defaultextension="*.pdf").name
+print()
+if(name == ""):
+    exit()
 while(True):
     try:
-        pdf = cairosvg.svg2pdf(url=url.format(i),scale=20)
+        pdf = cairosvg.svg2pdf(url=url.format(i), scale=20)
         merger.append(io.BytesIO(pdf))
-        print("{}".format(i))
+        print("\rDownloading page {}".format(i), end='')
     except urllib.error.HTTPError:
-        print("{} pages".format(i-1))
+        print("\rDownloaded {} pages".format(i-1), end='')
         break
-    i+=1
-merger.write(name + ".pdf")
-
+    i += 1
+merger.write(name)
